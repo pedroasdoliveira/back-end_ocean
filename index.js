46e7f47d45edbef51f20c94742c9883986eef28a
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-06-21 12:06:03 -0300

    part 1 finish

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-const herois = ["MUlher-Maravilha", "Superman", "Batman"]
+const herois = ["Mulher-Maravilha", "Superman", "Batman"];
 
-app.get('/herois', function (req, res) {
-    res.send(herois);
+app.get("/herois", function (req, res) {
+  res.send(herois.filter(Boolean));
+});
+
+app.get('/herois/:id', function (req, res) {
+  const id = req.params.id;
+  const item = herois[id - 1];
+
+  res.send(item);
+})
+
+app.post("/herois", function (req, res) {
+  const item = req.body.nome
+  herois.push(item)
+
+  console.log(req.body)
+  res.send("Item inserido com sucesso")
+});
+
+app.put('/herois/:id', function (req, res) {
+  const id = req.params.id;
+  const item = req.body.nome;
+
+  herois[id - 1] = item
+
+  res.send("Item atualizado")
+})
+
+app.delete('/herois/:id', function (req, res) {
+  const id = req.params.id;
+
+  delete herois[id - 1]
+
+  res.send('Item removido com sucesso')
 })
 
 app.listen(3000, () =>
